28daec2a91a7ae7fdfba33e08faaf3ebee55f61c
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-01-31 19:07:14 +0000

    Actually properly remove build_set()

--- tests.c
+++ tests.c
@@ -16,30 +16,6 @@ bool test_set_valid() {
     return (should_be_true && !should_be_false);
 }
 
-// bool test_build_set() {
-//     key k[9] = { 2, 3, 5, 1, 3, 1, 0, 0, 0 };
-//     set s[25] = {};
-//     // the expected set
-//     set e[25] = { 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0 };
-//     // call build_set
-//     build_set(k, s);
-//     // check if equal to expected set
-//     for(uint8_t i = 0; i < 25; i++) {
-//         if(s[i] != e[i]) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// bool test_build_set_is_valid() {
-//     // test that sets built with build_set() successfully validate as true by set_valid()
-//     key k[9] = { 1, 2, 1, 3, 5, 1, 2, 3, 0 };
-//     set s[25] = {};
-//     build_set(k, s);
-//     return set_valid(k, s);
-// }
-
 bool test_next_set() {
     // test that next_set() can iterate a set
     key k[9] = {}; // dummy for now
