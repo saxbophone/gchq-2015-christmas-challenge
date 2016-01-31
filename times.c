d0a234d1a54b8d7b8b8728f7d80b5df040227127
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-01-31 20:44:12 +0000

    Amend time test

--- times.c
+++ times.c
@@ -1,16 +1,15 @@
-#include <stdbool.h>
-#include <stdint.h>
+#include <stdlib.h>
 
 #include "crack.h"
 
 
 int main(int argc, char const *argv[]) {
-    // roughly time how long it takes to check if a set is valid and iterate, 2^25 times
-    key k[9] = { 1, 7, 3, 1, 1, 2, 0, 0, 0 };
-    set s[25] = { 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0 };
-    for(uint64_t i = 0; i < 33554432; i++) {
-        set_valid(k, s);
-        next_set(k, s, s);
-    }
+    // roughly time how long it takes to find all the valid patterns for a row
+    key_set r = {
+        .k = { 1, 7, 3, 1, 1, 2, 0, 0, 0 },
+        .s = { 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0 },
+    };
+    set_combos results = find_valid_sets(r.k, r.s);
+    free(results.sets);
     return 0;
 }
