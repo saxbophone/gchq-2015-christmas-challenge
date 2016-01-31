cee460c59bc3e988f4bb404fd6e9beaaa5618284
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-01-31 14:35:30 +0000

    Refactor count.c to use the re-implemented functions in crack.c Added nice table output of results.

--- count.c
+++ count.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "crack.h"
 
@@ -11,41 +12,34 @@ int main(int argc, char const *argv[]) {
     // show the number of possible combinations for each row and column of a grid
     printf("Finding valid settings for all Rows\n");
     printf("===================================\n");
+    printf("| Row    | Possible Combinations  |\n");
+    printf("===================================\n");
     for(uint8_t i = 0; i < 25; i++) {
-        printf("======== Row %d: ========\n", i+1);
+        printf("| %02d     | ", i+1);
+        fflush(stdout);
         key k[9] = {};
         set s[25] = {};
         row_to_set(PUZZLE, i, s, k);
-        uint64_t found = 0;
-        if(set_valid(k, s)) {
-            found++;
-        }
-        for(uint64_t j = 0; j < 33554432; j++) {
-            next_set(k, s, s);
-            if(set_valid(k, s)) {
-                found++;
-            }
-        }
-        printf("%lu Possible Settings\n", found);
+        set_results results = find_valid_sets(k, s);
+        printf("%06lu                 |\n", results.count);
+        free(results.sets);
     }
+    printf("===================================\n\n");
+
     printf("Finding valid settings for all Cols\n");
     printf("===================================\n");
+    printf("| Col    | Possible Combinations  |\n");
+    printf("===================================\n");
     for(uint8_t i = 0; i < 25; i++) {
-        printf("======== Col %d: ========\n", i+1);
+        printf("| %02d     | ", i+1);
+        fflush(stdout);
         key k[9] = {};
         set s[25] = {};
         col_to_set(PUZZLE, i, s, k);
-        uint64_t found = 0;
-        if(set_valid(k, s)) {
-            found++;
-        }
-        for(uint64_t j = 0; j < 33554432; j++) {
-            next_set(k, s, s);
-            if(set_valid(k, s)) {
-                found++;
-            }
-        }
-        printf("%lu Possible Settings\n", found);
+        set_results results = find_valid_sets(k, s);
+        printf("%06lu                 |\n", results.count);
+        free(results.sets);
     }
+    printf("===================================\n\n");
     return 0;
 }
